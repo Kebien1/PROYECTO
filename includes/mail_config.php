47785e1bb85117c6027425c5724f7a4932f9c1c8
Kebien1 <nunezortizkevin@gmail.com>
2025-12-10 13:53:38 -0400

    f

--- includes/mail_config.php
+++ includes/mail_config.php
@@ -6,17 +6,19 @@ use PHPMailer\PHPMailer\Exception;
 require_once __DIR__ . '../../phpmailer/PHPMailer.php';
 require_once __DIR__ . '../../phpmailer/SMTP.php';
 require_once __DIR__ . '../../phpmailer/Exception.php';
-
 function crearMailer(): PHPMailer {
     $mail = new PHPMailer(true);
     $mail->isSMTP();
     $mail->Host = 'smtp.gmail.com';
     $mail->SMTPAuth = true;
-    $mail->Username = 'colombiamasterg@gnail.com';
+    
+    $mail->Username = 'colombiamasterg@gmail.com'; 
     $mail->Password = 'zzpz wehx kbnw mqek';
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
     $mail->Port = 587;
     $mail->CharSet = 'UTF-8';
-    $mail->setFrom('aqui igual su correo electronico', 'PrograWeb I');
+    
+    $mail->setFrom('colombiamasterg@gmail.com', 'PrograWeb I');
+    
     return $mail;
 }
